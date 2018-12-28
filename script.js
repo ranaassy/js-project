d9103d80cf22e0ba7e95d1a945f4d8a67e32d3f7
[rana assy] <[accrana20@gmail.com]>
2018-12-28 23:22:00 +0200

    edit code

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var keys = document.querySelectorAll('#calculator span');
 var operators = ['+', '-', 'x', '÷'];
-var decimalAdded = false;
 
 for (var i = 0; i < keys.length; i++) {
     keys[i].onclick = function(e) {
@@ -11,42 +10,15 @@ for (var i = 0; i < keys.length; i++) {
 
         if (btnVal == 'C') {
             input.innerHTML = '';
-            decimalAdded = false;
         }
         // If eval key is pressed
         else if (btnVal == '=') {
-            var equation = inputVal;
-            var lastChar = equation[equation.length - 1];
+            if (inputVal)
+                input.innerHTML = eval(inputVal);
 
-            equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
-
-            if (operators.indexOf(lastChar) > -1 || lastChar == '.')
-                equation = equation.replace(/.$/, '');
-
-            if (equation)
-                input.innerHTML = eval(equation);
-
-            decimalAdded = false;
-        } else if (operators.indexOf(btnVal) > -1) {
-
-            var lastChar = inputVal[inputVal.length - 1];
-
-            if (inputVal != '' && operators.indexOf(lastChar) == -1)
-                input.innerHTML += btnVal;
-
-            else if (inputVal == '' && btnVal == '-')
-                input.innerHTML += btnVal;
-
-            if (operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
-                input.innerHTML = inputVal.replace(/.$/, btnVal);
-            }
-
-            decimalAdded = false;
         } else if (btnVal == '.') {
-            if (!decimalAdded) {
-                input.innerHTML += btnVal;
-                decimalAdded = true;
-            }
+            input.innerHTML += btnVal;
+            var decimalAdded = true;
         } else {
             input.innerHTML += btnVal;
         }
